75106b147bd04af25f929bb9da84db45a04b3766
Cristhian Steffano Díaz Piminchumo <steffanod948@gmail.com>
2026-07-10 15:37:10 -0500

    Añadir carpeta Sistema (SistemaDelivery)
    Lógica de negocio y orquestación (SistemaDelivery.cs). Coordina formularios y estructuras de datos.

--- Sistema/SistemaDelivery.cs
+++ Sistema/SistemaDelivery.cs
@@ -1,4 +1,4 @@
-﻿using RestaurantIngenieriaTrujillo.Estructuras.ABB;
+using RestaurantIngenieriaTrujillo.Estructuras.ABB;
 using RestaurantIngenieriaTrujillo.Estructuras.ColaPrioridad;
 using RestaurantIngenieriaTrujillo.Estructuras.Grafos;
 using RestaurantIngenieriaTrujillo.Estructuras.ListaCircular;
@@ -8,31 +8,40 @@ using RestaurantIngenieriaTrujillo.Estructuras.Pila;
 using RestaurantIngenieriaTrujillo.Estructuras.ListaProductos;
 using RestaurantIngenieriaTrujillo.Estructuras.Cola;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RestaurantIngenieriaTrujillo.Sistema
 {
+    /// Clase central del sistema que gestiona todas las estructuras de datos.
+    /// Actúa como contenedor de instancias estáticas para acceso global.
     internal class SistemaDelivery
     {
+        /// Lista simple de clientes registrados en el sistema.
         public static ListaClientes ListaClientes = new ListaClientes();
 
+        /// Lista de productos disponibles en el menú del restaurante.
         public static ListaProductos ListaProductos = new ListaProductos();
 
+        /// Lista circular de repartidores disponibles para entregas.
         public static ListaCircularRepartidores ListaRepartidores = new ListaCircularRepartidores();
 
+        /// Cola FIFO de pedidos con prioridad normal.
         public static ColaPedidos ColaPedidos = new ColaPedidos();
 
+        /// Cola de prioridad para pedidos prioritarios.
         public static ColaPrioridadPedidos ColaPrioridad = new ColaPrioridadPedidos();
 
-        public static ListaDobleHistorial Historial =new ListaDobleHistorial();
+        /// Lista doble para registrar historial de cambios de estado de pedidos.
+        public static ListaDobleHistorial Historial = new ListaDobleHistorial();
 
+        /// Pila LIFO que almacena el historial de cambios para deshacer operaciones.
         public static PilaPedidos PilaCambios = new PilaPedidos();
 
+        /// Árbol binario de búsqueda que almacena todos los pedidos para búsquedas eficientes.
         public static ArbolPedidos ArbolPedidos = new ArbolPedidos();
 
+        /// Grafo que representa las rutas y zonas de cobertura del servicio de delivery.
         public static GrafoRutas Grafo = new GrafoRutas();
     }
 }
