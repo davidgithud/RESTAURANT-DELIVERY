2e5855a6e8fbd7a35e131361edcb8612a534be81
Cristhian Steffano Díaz Piminchumo <steffanod948@gmail.com>
2026-07-10 15:39:08 -0500

    Añadir diseñadores y recursos de Formularios
    Archivos .Designer.cs y .resx asociados a los formularios.

--- FrmPrincipal.cs
+++ FrmPrincipal.cs
@@ -1,4 +1,6 @@
 using RestaurantIngenieriaTrujillo.Formularios;
+using RestaurantIngenieriaTrujillo.Entidades;
+using RestaurantIngenieriaTrujillo.Sistema;
 
 namespace RestaurantIngenieriaTrujillo
 {
@@ -54,12 +56,48 @@ namespace RestaurantIngenieriaTrujillo
         }
         private void salirToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Close();
+            DialogResult resultado = MessageBox.Show(
+                "¿Desea salir de la aplicación?",
+                "Confirmar Salida",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (resultado == DialogResult.Yes)
+            {
+                Close();
+            }
         }
 
         private void FrmPrincipal_Load(object sender, EventArgs e)
         {
+            InicializarGrafo();
+        }
 
+        private void InicializarGrafo()
+        {
+            if (SistemaDelivery.Grafo.CantidadZonas() > 0)
+                return;
+
+            SistemaDelivery.Grafo.AgregarZona(new Zona(0, "Restaurant"));
+            SistemaDelivery.Grafo.AgregarZona(new Zona(1, "Centro"));
+            SistemaDelivery.Grafo.AgregarZona(new Zona(2, "California"));
+            SistemaDelivery.Grafo.AgregarZona(new Zona(3, "El Recreo"));
+            SistemaDelivery.Grafo.AgregarZona(new Zona(4, "Primavera"));
+            SistemaDelivery.Grafo.AgregarZona(new Zona(5, "San Andrés"));
+            SistemaDelivery.Grafo.AgregarZona(new Zona(6, "Las Quintanas"));
+            SistemaDelivery.Grafo.AgregarZona(new Zona(7, "Palermo"));
+            SistemaDelivery.Grafo.AgregarZona(new Zona(8, "El Golf"));
+            SistemaDelivery.Grafo.AgregarZona(new Zona(9, "Víctor Larco"));
+
+            SistemaDelivery.Grafo.AgregarConexion(0, 1, 2);
+            SistemaDelivery.Grafo.AgregarConexion(0, 2, 4);
+            SistemaDelivery.Grafo.AgregarConexion(1, 3, 2);
+            SistemaDelivery.Grafo.AgregarConexion(2, 4, 3);
+            SistemaDelivery.Grafo.AgregarConexion(3, 5, 2);
+            SistemaDelivery.Grafo.AgregarConexion(4, 6, 2);
+            SistemaDelivery.Grafo.AgregarConexion(5, 7, 3);
+            SistemaDelivery.Grafo.AgregarConexion(6, 8, 2);
+            SistemaDelivery.Grafo.AgregarConexion(8, 9, 4);
         }
 
         private void toolStripStatusLabel3_Click(object sender, EventArgs e)
